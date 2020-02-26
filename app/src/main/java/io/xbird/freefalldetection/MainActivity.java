c3d8943c8283e11e3b5450ae230889a3b3a91844
JAbozarOid <araghibdoust@hotmail.com>
2020-02-26 15:15:02 +0330

    push notif when free fall happend

--- app/src/main/java/io/xbird/freefalldetection/MainActivity.java
+++ app/src/main/java/io/xbird/freefalldetection/MainActivity.java
@@ -1,31 +1,27 @@
 package io.xbird.freefalldetection;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
 import android.content.Intent;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import io.xbird.freefalldetection.Service.FallDetectionService;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         Log.d(TAG, "onCreate: Service Called");
         Intent serviceIntent = new Intent(this, FallDetectionService.class);
         startService(serviceIntent);
 
     }
-
 
 }
---
+++ app/src/main/java/io/xbird/freefalldetection/Service/FallDetectionService.java
@@ -0,0 +1,142 @@
+package io.xbird.freefalldetection.Service;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
+import io.xbird.freefalldetection.R;
+
+
+public class FallDetectionService extends Service implements SensorEventListener {
+
+    //logt
+    private static final String TAG = "FallDetectionService";
+    private SensorManager mSensorManager;
+    private Sensor accelerometer;
+
+    public static boolean isLastStateFall = false;
+    public static boolean isCurrentStateFall = false;
+    public static long startTime = 0;
+    public static long stopTime = 0;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        Log.d(TAG, "onCreate: Initializing Sensor Services");
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, "Service Started", Toast.LENGTH_SHORT).show();
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if (mSensorManager != null) {
+            accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            mSensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
+            Log.d(TAG, "onStartCommand: Registered accelerometer listener");
+        }
+
+        return Service.START_STICKY;
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+        isCurrentStateFall = isFall(sensorEvent);
+
+        if (!isLastStateFall && isCurrentStateFall) {
+            Log.d(TAG, "ABOUZAR : FALL START");
+            startTime = sensorEvent.timestamp;
+
+        }
+
+        if (isLastStateFall && !isCurrentStateFall) {
+            Log.d(TAG, "ABOUZAR : FALL END");
+            stopTime = sensorEvent.timestamp;
+            Log.d(TAG, "ABOUZAR : fall time" + " " + ((stopTime - startTime) / 1000000) + " milliSeconds");
+            sendLocalPushNotif();
+
+        }
+
+        isLastStateFall = isCurrentStateFall;
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    public boolean isFall(SensorEvent sensorEvent) {
+
+        float x = sensorEvent.values[0];
+        float y = sensorEvent.values[1];
+        float z = sensorEvent.values[2];
+        float r = (float) Math.sqrt(x * x + y * y + z * z);
+
+        return r < 0.5;
+    }
+
+    private void sendLocalPushNotif() {
+
+        Log.d(TAG, "sendLocalPushNotif: ");
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+            startMyOwnForeground();
+        else
+            startForeground(1, new Notification());
+
+    }
+
+
+
+    private void startMyOwnForeground(){
+        String NOTIFICATION_CHANNEL_ID = "io.xbird.freefalldetection";
+        String channelName = "My Background Service";
+        NotificationChannel chan = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            chan = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_NONE);
+            chan.setLightColor(Color.BLUE);
+            chan.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+        }
+
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        assert manager != null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            manager.createNotificationChannel(chan);
+        }
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+        Notification notification = notificationBuilder.setOngoing(true)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("Free Fall Happened")
+                .setPriority(NotificationManager.IMPORTANCE_MIN)
+                .setCategory(Notification.CATEGORY_SERVICE)
+                .build();
+        startForeground(2, notification);
+    }
+
+}
